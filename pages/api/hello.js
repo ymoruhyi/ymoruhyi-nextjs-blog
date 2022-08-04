f858e8a4c1bf625c49c51bbae322b4755d4a9e1a
Yevhenii Moruhyi <ymoruhyi@cisco.com>
2022-08-04 17:20:30 -0400

    fix async

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,7 +1,7 @@
 import { unstable_getServerSession } from 'next-auth/next';
 import { authOptions } from './auth/[...nextauth]';
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
   const session = await unstable_getServerSession(req, res, authOptions);
 
   if (session) {
